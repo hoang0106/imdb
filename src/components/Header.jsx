7c760aa755c3081f6facb63306eb15d69d7ec066
hoang0106 <nguyen.d.hoang@gmail.com>
2023-07-28 16:56:47 -0700

    Create about page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import { AiFillHome } from 'react-icons/ai'
 import { BsFillInfoCircleFill } from 'react-icons/bs'
 import Link from 'next/link'
 import DarkModeSwitch from './DarkModeSwitch'
+import about from '@/app/about/page'
 
 export default function Header() {
   return (
