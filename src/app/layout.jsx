fdffbf7a522a3782b3789c6ad8df34160b3e5455
hoang0106 <nguyen.d.hoang@gmail.com>
2023-07-30 09:26:47 -0700

    add search ability

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import Header from '@/components/Header'
 import './globals.css'
 import Providers from './Providers'
 import NavBar from '@/components/NavBar'
+import SearchBox from '@/components/SearchBox'
 
 export default function RootLayout({ children }) {
   return (
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
           {/* Navbar */}
           <NavBar />
           {/* Search */}
+          <SearchBox />
           {children}
         </Providers>
       </body>
